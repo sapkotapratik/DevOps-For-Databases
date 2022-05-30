1afb0f41c322bd94e05705a6b7fcd23b2ff97db0
Pratik Sapkota <sapkotapratik053@gmail.com>
2022-05-30 16:42:09 +0530

    Update main.tf
    Add mysql config changes.

--- SQL-Server-Creation/main.tf
+++ SQL-Server-Creation/main.tf
@@ -4,25 +4,31 @@
 provider "azurerm" {
   features {}
 }
+resource "azurerm_resource_group" "devops_test" {
+  name     = "devops-example-resources"
+  location = "Central India"
+}
 
 resource "azurerm_mysql_server" "devopsdatabase" {
   name                = var.db_name
-  location            = var.location
-  resource_group_name = var.rg_name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 
-  administrator_login          = "mysqladmin"
-  administrator_login_password = var.password
+
+  administrator_login          = "pratikmysqladmin"
+  administrator_login_password = "RClearn@2022"
 
   sku_name   = "B_Gen5_2"
   storage_mb = 5120
   version    = "8.0"
-
+  
   auto_grow_enabled                 = true
   backup_retention_days             = 7
   geo_redundant_backup_enabled      = false
   infrastructure_encryption_enabled = false
   public_network_access_enabled     = true
-  ssl_enforcement_enabled           = false
+  ssl_enforcement_enabled           = true
+  ssl_minimal_tls_version_enforced  = "TLS1_2"
 }
 
 
